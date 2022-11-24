03e02a99e8bd524a96aff8962435bb40e03ff587
shakezula <lott.dylan@gmail.com>
2022-11-23 18:35:09 -0700

    WIP stripe integration

--- app.go
+++ app.go
@@ -10,7 +10,17 @@ import (
 	"os"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/stripe/stripe-go/v73"
+	"github.com/stripe/stripe-go/v73/charge"
 )
+
+// Config holds config values for the application
+type Config struct {
+	Port        string
+	InfluxURL   string
+	InfluxToken string
+	TemplateDir string
+}
 
 //go:embed templates/*
 var resources embed.FS
@@ -33,13 +43,53 @@ func main() {
 		log.Fatalf("failed influxDB health check: %v", err)
 	}
 
+	// serves the home page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]string{
 			"Region": os.Getenv("FLY_REGION"),
 			"Status": string(hc.Status),
 		}
-
 		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+
+	// charge is pinged by the Checkout route's credit card form
+	http.HandleFunc("/charge", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("charge hit: %+v", r.Form)
+		stripe.Key = os.Getenv("STRIPE_SECRET_KEY")
+
+		// Token is created using Stripe Checkout or Elements!
+		// Get the payment token ID submitted by the form:
+		token := r.FormValue("stripeToken")
+
+		params := &stripe.ChargeParams{
+			// TODO: charge for correct amount
+			Amount: stripe.Int64(999),
+			// TODO: charge for quantity to allow multiple unit orders
+			Currency:    stripe.String(string(stripe.CurrencyUSD)),
+			Description: stripe.String("Example charge"),
+		}
+		params.SetSource(token)
+
+		ch, err := charge.New(params)
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte(fmt.Sprintf("failed to charge: %+v", err)))
+			return
+		}
+
+		log.Printf("successfully charged: %+v", ch)
+
+		// redirect to ch.ReceiptURL
+		http.Redirect(w, r, ch.ReceiptURL, http.StatusMovedPermanently)
+	})
+
+	// checkout serves the credit card form
+	http.HandleFunc("/checkout", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Region": os.Getenv("FLY_REGION"),
+			"Status": string(hc.Status),
+		}
+		t.ExecuteTemplate(w, "checkout.html.tmpl", data)
 	})
 
 	http.HandleFunc("/buckets", func(w http.ResponseWriter, r *http.Request) {
